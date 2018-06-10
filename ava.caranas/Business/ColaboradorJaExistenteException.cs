b51c63eb4d566986be716f8fba465cdb7a9ebdef
Bruno Nogueira De Souza Medeiros <b.de.souza.medeiros@avanade.com>
2018-06-10 10:28:08 -0300

    Add ColaboradorBusiness Class

---
+++ ava.caranas/Business/ColaboradorJaExistenteException.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ava.caronas.Business {
+    public class ColaboradorJaExistenteException : Exception {
+        public ColaboradorJaExistenteException() {}
+        public override string Message => "ID já existente...";
+    }
+}
